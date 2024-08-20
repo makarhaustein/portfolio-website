799af360bdaf4f8fc2863efa585e3f2f02138baf
Makar Haustein <makarmaxha@gmail.com>
2024-08-20 22:19:04 +0200

    Changed Header, added underline animation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,21 @@
+
 export default function Header() {
     return (
         <div className='w-full mb-20'>
-            <div className='py-2 w-full absolute top-0 left-0 bg-white text-xl font-medium font-plexmono'>
+            <div className='p-1 w-full fixed top-0 left-0 sm:text-xl font-medium 
+                font-plexmono bg-white border-t-2 border-b-2 border-black'>
                 <ul className='m-auto flex justify-evenly max-w-4xl'>
-                    <li className='px-3 rounded-lg hover:bg-slate-100 hover:text-slate-900'>
-                        <a href=''>Home</a>
+                    <li className='px-3 rounded-lg'>
+                        <a href='' className='underline-animation'>Home</a>
                     </li>
-                    <li className='px-3 rounded-lg hover:bg-slate-100 hover:text-slate-900'>
-                        <a href=''>Projects</a>
+                    <li className='px-3 rounded-lg'>
+                        <a href='' className='underline-animation'>Projects</a>
                     </li>
-                    <li className='px-3 rounded-lg hover:bg-slate-100 hover:text-slate-900'>
-                        <a href=''>3D_Models</a>
+                    <li className='px-3 rounded-lg'>
+                        <a href='' className='underline-animation'>3D_Models</a>
                     </li>
-                    <li className='px-3 rounded-lg hover:bg-slate-100 hover:text-slate-900'>
-                        <a href=''>About_Me</a>
+                    <li className='px-3 rounded-lg'>
+                        <a href='' className='underline-animation'>About_Me</a>
                     </li>
                 </ul>
             </div>
