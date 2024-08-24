e5fbe0d354b75eb6c4f556bc006a40ac67bed21e
Makar Haustein <makarmaxha@gmail.com>
2024-08-24 23:31:04 +0200

    Intro.jsx should not be a component, changed HomePage accordingly

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,18 @@
-import Intro from '../components/Intro.jsx'
+import Terminal from '../components/Terminal.jsx'
 
 function HomePage() {
 
     return (
         <div className='flex items-center w-full flex-col'>
-            <Intro />
+            <Terminal>
+                <h1 className='font-plexmono text-4xl'>Welcome!</h1>
+                <h2 className='font-plexmono text-xl'>
+                    Here you can find out more about me and my projects!
+                </h2>
+                <h2 className='font-plexmono text-xl'>
+                    This website is currently being worked on!
+                </h2>
+            </Terminal>
         </div>
     )
 }
