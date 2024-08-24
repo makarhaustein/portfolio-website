07a4b5e6dc34957ec05681d8519cc16735c96978
Makar Haustein <makarmaxha@gmail.com>
2024-08-25 01:12:23 +0200

    Increased min height, added gh icon into footer and changed visual

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,19 @@
 export default function Footer() {
     return (
-        <div class='bg-white w-full left-0 h-10'>
-            <div class='mt-10 pr-2 p-2 space-x-2 bg-white rounded-t-md text-black 
-                    text-right flex justify-end'>
+        <div className='bg-slate-900 w-full left-0 min-h-10 py-8 flex 
+                        flex-col justify-center items-center
+                        text-white font-poppins'>
+            <h2 className='text-xl'>Socials</h2>
+            <div className='flex justify-center'>
+                <a href='https://github.com/makarhaustein' className='flex items-center'>
+                    <img className='h-3 mr-1' src='../../public/images/icons/github-mark-white.png' />
+                    <p>GitHub</p>
+                </a>
+
             </div>
+            <p className='text-[10px] text-slate-700'>
+                Copyright © 2024 Makar Haustein. All Rights Reserved.
+            </p>
         </div>
     );
 }
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,12 +3,12 @@ import Footer from './Footer.jsx';
 
 export default function Layout({ children }) {
     return (
-        <>
+        <div className='flex flex-col min-h-screen'>
             <Header />
-            <main>
+            <main className='flex-grow'>
                 {children}
             </main>
             <Footer />
-        </>
+        </div>
     );
 }
